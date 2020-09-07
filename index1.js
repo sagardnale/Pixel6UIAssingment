214cfc3accedad62755bb59bd64a2ccac5d6778b
Sagar Dashrath Nale <35920963+sagardnale@users.noreply.github.com>
2020-09-07 12:57:13 +0530

    js file for first page

--- index1.js
+++ index1.js
@@ -64,9 +64,65 @@ function invalidEmail(message){
  * 1000-972=28
  * I have given 28 numbers have Spam String
  */
+function takePhoneNumber(phoneNum){
+  var curval = phoneNum.value;
+  var e = curval.slice(-1);
+  if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+    return false;
+  }
+  var curchr = curval.length;
+  console.log(curchr);
+  if (curchr == 3 && curval.indexOf("(") <= -1) {
+      document.getElementById("phoneNumber").value= "(" +curval + ")" + "-";
+    if(curval >= 621 && curval<=799){
+        document.getElementById("myImg").src= "jio.png";
+        document.getElementById("comapnyName").innerHTML="Jio";
+    } else if(curval >= 801 && curval<=920){
+        document.getElementById("myImg").src= "idea.png";
+        document.getElementById("comapnyName").innerHTML="Idea";
+    }else if(curval >= 921 && curval<=999){
+        document.getElementById("myImg").src="vodaphone.png";
+        document.getElementById("comapnyName").innerHTML="Vodaphone";
+    }else{
+        document.getElementById("myImg").src="invalid.png";
+        document.getElementById("comapnyName").innerHTML="Spam";
+
+        phonevalidate="Spam";
+    }
+  } else if (curchr == 4 && curval.indexOf("(") > -1) {
+    document.getElementById("phoneNumber").value= curval + ")-";
+  } else if (curchr == 5 && curval.indexOf(")") > -1) {
+    document.getElementById("phoneNumber").value= curval + "-";
+  } else if (curchr == 9) {
+    //Here is the logic for the state and Uts range
+    var lastdigits = curval.substring(curval.length-3);
+    
+    var stateandUTS = ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Orissa","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli & Daman and Diu","National Capital Territory of Delhi","Jammu and Kashmir","Ladakh","Lakshadweep","Puducherry"];
+    
+     var start = Math.floor(lastdigits/27);
+    
+    if(lastdigits >= start*27 && lastdigits < (start+1)*27 && lastdigits <=972) {
+        document.getElementById("stateUTs").innerHTML= ','+stateandUTS[start-1];
+    }else{
+        document.getElementById("stateUTs").innerHTML= ','+"Spam";
+    }
+    document.getElementById("phoneNumber").value= curval + "-";  
+  } 
+  if(curchr==10){
+    curval = curval.slice(0,9);
+    document.getElementById("phoneNumber").value=curval;
+  }else if(curchr==6){
+    curval = curval.slice(0,4);
+    document.getElementById("phoneNumber").value=curval;
+  }else if(curval < 2){
+    document.getElementById("phoneNumber").value="";
+  }
+  return;
+}
+
 
  //Phone number validation
-function takePhoneNumber(phoneNum){
+/*function takePhoneNumber(phoneNum){
   var number = phoneNum.value;
   var lastchar = number.slice(-1);
   var numberlength = number.length;
@@ -108,3 +164,4 @@ function takePhoneNumber(phoneNum){
   document.getElementById("phoneNumber").value=number;
   return ;
 }
+*/
